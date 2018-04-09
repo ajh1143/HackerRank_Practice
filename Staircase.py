364f9804360cc16909aa683e1ee549436ddf9403
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-04-09 14:57:29 -0700

    Create Staircase.py

---
+++ Staircase.py
@@ -0,0 +1,28 @@
+#Write a program that prints a staircase of n size 
+#Create a staircase built with hashes and spaces, whose width and height are both equal to n 
+#and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+"""
+Ex. Output of n == 6
+     # 
+    ##
+   ###
+  ####
+ #####
+######
+""" 
+
+#!/bin/python3
+
+import os
+import sys
+
+def staircase(n):
+    spaces = " "
+    hashes = "#"
+    for x in range(n):
+        print((n-x-1)*spaces + ((x+1)*hashes))
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
