47d78ee9bf3ef0ed37b3c049c65a10c957b50e7e
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2018-04-11 13:21:09 -0700

    Update RunnerUp.py
    Added Second solution using list comprehension approach

--- RunnerUp.py
+++ RunnerUp.py
@@ -1,10 +1,20 @@
 #Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. 
 #You are given 'n' scores. Store them in a list and find the score of the runner-up.
 
+#Boolean Loop Solution Approach
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
     arrMax = max(arr)
     while arrMax in arr:
           arr.remove(arrMax)
+    print(max(arr))
+    
+    
+#List Comprehension Approach
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arrMax = max(arr)
+    arr = [score for score in arr if score != arrMax]
     print(max(arr))
